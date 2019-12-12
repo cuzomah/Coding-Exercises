134f99bbba83968201a8a1671e481d7e358a1210
cuzomah <chibueze.uzomah4302@outlook.com>
2019-12-12 10:41:53 -0500

    Got half way through the WorkingWithStrings tutorial

---
+++ VideoTutorials/WorkingWithStrings.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace WorkingWithStrings
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/*string myString = "My \"so joyous\" life";
+			Console.WriteLine (myString);
+			Console.WriteLine ();
+			string myLine = "What if a need a \nNew Line?";
+			Console.WriteLine (myLine);*/
+
+/*			string myDrive = "The all knowing drive: the c:\\ drive";
+	        or this method:
+			string myDrive = @"The all knowing drive: the c:\ drive"; 
+		     @ cancels out backlash escape operation 
+			Console.WriteLine (myDrive);
+
+			Predefined formating techniques
+
+			string myString = String.Format ("{0:C}", 400.25);         
+			Console.WriteLine (myString);
+			string hugeNumber = String.Format ("{0:N}", 8234535226);
+			Console.WriteLine (hugeNumber);
+			string percentage = String.Format ("{0:P}", .576);
+			Console.WriteLine (percentage);
+			A way to create a custom format is below
+
+			string randomNumber = String.Format ("Phone number:{0:(###)###-####}", 3018764321);
+			Console.WriteLine (randomNumber);*/
+
+			string favoriteLyric = "";
+
+
+		}
+	}
+}
